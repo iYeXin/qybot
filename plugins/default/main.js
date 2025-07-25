bcb0ee5ef1a8edf9df3ead901297bbf681bcb5e1
iYeXin <3398097759@qq.com>
2025-07-26 00:50:38 +0800

    默认插件修复bug

--- plugins/default/main.js
+++ plugins/default/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    helloPlugin: {
+    defaultPlugin: {
         async main(msgType, msgContent, senderOpenid) {
             return `你好，${senderOpenid}！你说的是: ${msgContent}（请在实际安装插件后删除此默认插件）`;
         },
